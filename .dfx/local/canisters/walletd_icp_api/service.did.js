f6e78d63a569fec267662ab431e10f89f6d016ca
AslanPonies <109072739+AslanPonies@users.noreply.github.com>
2025-05-19 17:51:02 +0200

    Add all files including dfx and .dfx

--- .dfx/local/canisters/walletd_icp_api/service.did.js
+++ .dfx/local/canisters/walletd_icp_api/service.did.js
@@ -1,34 +1,3 @@
-export const idlFactory = ({ IDL }) => {
-  const IcpTransaction = IDL.Record({
-    'to' : IDL.Principal,
-    'signature' : IDL.Vec(IDL.Nat8),
-    'from' : IDL.Principal,
-    'memo' : IDL.Opt(IDL.Nat64),
-    'amount' : IDL.Nat64,
-  });
-  return IDL.Service({
-    'approve' : IDL.Func([IDL.Text, IDL.Text, IDL.Nat64], [IDL.Nat64], []),
-    'balance' : IDL.Func([IDL.Text], [IDL.Nat64], []),
-    'generate_address' : IDL.Func([], [IDL.Text], ['query']),
-    'new_wallet' : IDL.Func([], [], ['oneway']),
-    'resolve_did' : IDL.Func([IDL.Text], [IDL.Opt(IDL.Principal)], ['query']),
-    'swap_icp_to_btc' : IDL.Func(
-        [IDL.Principal, IDL.Text, IDL.Nat64],
-        [],
-        ['oneway'],
-      ),
-    'sync_balance' : IDL.Func([], [], ['oneway']),
-    'transaction_history' : IDL.Func(
-        [IDL.Text],
-        [IDL.Vec(IcpTransaction)],
-        ['query'],
-      ),
-    'transfer' : IDL.Func([IDL.Text, IDL.Text, IDL.Nat64], [], ['oneway']),
-    'transfer_from' : IDL.Func(
-        [IDL.Text, IDL.Text, IDL.Text, IDL.Nat64],
-        [IDL.Nat64],
-        [],
-      ),
-  });
-};
-export const init = ({ IDL }) => { return []; };
+version https://git-lfs.github.com/spec/v1
+oid sha256:c8ce2696a288a3775b231074f6228b30c57beb22218f01578ce0b50828fe622b
+size 1131
